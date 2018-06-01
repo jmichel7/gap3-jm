ae231464e0a975108c98c582dc34d84794480b0e
Christian Stump <christian.stump@gmail.com>
2018-06-01 09:09:50 +0200

    working on the aux file

--- sage/cheviedata/cmplxaux.py
+++ sage/cheviedata/cmplxaux.py
@@ -1,6 +1,14 @@
 from collections import defaultdict 
 from sage.rings.universal_cyclotomic_field import E
 
+CharTableA = CharTableB = CharTableD = lambda *args: {}
+CharTableSymmetric = CharTableWeylA = CharTableWeylB = CharTableWeylD = {}
+
+ChevieData = defaultdict(dict)
+ChevieData["compat"] = defaultdict(dict)
+ChevieData["compat"]["CharTableB"] = CharTableB
+ChevieData["compat"]["HeckeCharTableB"] = CharTableB
+
 def ER(n):
   if n == 0:
       return 0
@@ -51,6 +59,16 @@ def GAPMul(A,B):
             else:
                 B = vector(B)
         return A*B
+
+#def GAPMin(A,B):
+    #if isinstance(A,list) and isinstance(B,int):
+        #return [a-B for a in A]
+    #if isinstance(A,list) and isinstance(B,list):
+        #return [a-b for a,b in zip(A,B)]
+    #if isinstance(A,int) and isinstance(B,list):
+        #return [A-b for b in B]
+    #if isinstance(A,int) and isinstance(B,int):
+        #return a-b
 
 def Reflection(root, coroot):
     g = vector(root)
@@ -147,7 +165,9 @@ def PermutationsSimpleReflections(C, Phi):
         gens.append( Permutation([Phi.index(beta)+1 for beta in PhiNew]) )
     return gens
 
-def RootsCartanCox(C):
+def RootsCartan(C):
+    if isinstance(C,list):
+        C = Matrix(C)
     # Cox not Weyl
     n = C.nrows()
     R = identity_matrix(n).rows()
@@ -168,7 +188,6 @@ def CollectBy(A,B):
             if x == i:
                 out[-1].append(A[j])
     return [ tuple(x) for x in out ]
-
 
 def Replace(*args):
     assert len(args) % 2 == 1
@@ -204,3 +223,60 @@ def find_first_sublist(seq, sublist, start=0):
     for index in range(start, len(seq)):
         if seq[index:index+length] == sublist:
             return index, index+length
+
+def import_all():
+    import os
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    errs = []
+    for f in files:
+        if f != "cmplxaux.py":
+            #print "Importing %s"%f
+            try:
+                load(f)
+            except StandardError as err:
+                print "Importing %s failed!"%f
+                print "Error message:"
+                print err
+                errs.append(f)
+    return errs
+
+def DiagonalMat(*args):
+    M = args[0]
+    M = Matrix(M)
+    for y in args[1:]:
+        y = Matrix(y)
+        M = M.block_sum(y)
+    return M
+
+def ReadChv(*args):
+    return
+
+def ComplexConjugate(X):
+    from sage.functions.other import conjugate
+    if isinstance(X,list):
+        return [ComplexConjugate(x) for x in X]
+    else:
+        return conjugate(X)
+
+def Denominator(X):
+    if hasattr(X,"denominator"):
+        return X.denominator()
+    else:
+        return X
+
+def Numerator(X):
+    if hasattr(X,"numerator"):
+        return X.numerator()
+    else:
+        return X
+
+def eptio2(t):
+    r=ChevieData[t]["GeneratingRoots"]
+    rbar=ComplexConjugate(r)
+    e=ChevieData[t]["EigenvaluesGeneratingReflections"]
+    e=[1-x for x in map(lambda x: ER(Denominator(x))**Numerator(x),e)]
+    e=map(lambda i: GAPDiv(GAPMul(e[i-1],rbar[i-1]),GAPMul(rbar[i-1],r[i-1])),range(1,len(e)+1))
+    return map(lambda x: map(lambda y: GAPMul(x,y),r),e)
+
+def SPrint(*args):
+    return "".join(str(a) for a in args)
